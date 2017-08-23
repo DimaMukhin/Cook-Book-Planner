c5bc3c6482a0084bb37379b847f920c215e2c3f4
DimaMukhin <dima9348@gmail.com>
2017-08-23 13:21:28 -0500

    implemented "add to shopping cart" for recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,40 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('First test recipe', 'first test description', 'http://images.media-allrecipes.com/images/58656.png'),
-    new Recipe('Second test recipe', 'second test description', 'https://eat24hours.com/files/cuisines/v4/thai.jpg?e24v=103?e24v=178?e24v=178')
+    new Recipe(
+      'Cheeseburger',
+      'Not kosher',
+      'http://images.media-allrecipes.com/images/58656.png',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bun', 1),
+        new Ingredient('Cheese', 1)
+      ]),
+    new Recipe(
+      'Veggie Roll',
+      'Delicious',
+      'https://eat24hours.com/files/cuisines/v4/thai.jpg?e24v=103?e24v=178?e24v=178',
+      [
+        new Ingredient('Tomatoe', 4),
+        new Ingredient('Mushroom', 5)
+      ])
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
